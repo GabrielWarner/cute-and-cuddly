523f121e843856445cefdbc4d8c017d7cd1db963
alonpatashnik <alonp714@gmail.com>
2022-07-08 10:49:42 -0700

    add working api for cat image by breed

--- catScript.js
+++ catScript.js
@@ -2,10 +2,20 @@ var cat = document.getElementById('catImage')
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
+    //var instances = M.Dropdown.init(elems, options);
 });
 
 var breed = "beng"
-var catUrl = 'https://api.thecatapi.com/v1/breeds/search?q='+breed+'&api_key=f0f11a12-d477-4d44-ae91-5f773cb8183f';
+var catUrl = 'https://api.thecatapi.com/v1/images/search?breed_ids='+ breed
 
+function catPicByBreed() {
+    
+    var catImg = document.createElement('img')
+    fetch(catUrl)
+    .then((response) => response.json())
+    .then((data) => catImg.setAttribute('src', data[0].url))
+    catImg.setAttribute('class', 'col l8')
+    cat.append(catImg)
+}
 
+catPicByBreed()
