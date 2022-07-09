fe557459fe05915afb90345269fd7d5fcedcb7dd
rasvindra <rasvindra@hotmail.com>
2022-07-09 14:21:52 -0700

    added array for rndm dog info

--- compare.js
+++ compare.js
@@ -7,10 +7,9 @@ var catsImage = document.getElementById("catImage");
 var catsName = document.getElementById("catNameApi");
 var catsLife = document.getElementById("catLifeApi");
 var catsInfo = document.getElementById("catInfoApi");
-<<<<<<< HEAD
 var homeMode = document.getElementById('homeBtn')
-=======
-var homeMode = document.getElementById('homeBtn');
+var dogArrWorking = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","28","29","30","31","32","33","34","36","38","41","42","43","45","47","48","51","52","53","54","55","56","57","58","59","61","67","68","69","70","71","76","78","79","80","81","201"]
+
 var breedArr = ['Abyssinian', 'Aegean', 'American Bobtail', 'American Curl', 'American Shorthair',
     'American Wirehair', 'Arabian Mau', 'Australian Mist', 'Balinese', 'Bambino', 'Bengal', 'Birman',
     'Bombay', 'British Longhair', 'British Shorthair', 'Burmese', 'Burmilla', 'California Spangled',
@@ -22,7 +21,7 @@ var breedArr = ['Abyssinian', 'Aegean', 'American Bobtail', 'American Curl', 'Am
     'Russian Blue', 'Savannah', 'Scottish Fold', 'Selkirk Rex', 'Siamese', 'Siberian', 'Singapura',
     'Snowshoe', 'Somali', 'Sphynx', 'Tonkinese', 'Toyger', 'Turkish Angora', 'Turkish Van', 'York Chocolate'
 ];
->>>>>>> 226e9dc53a94c8e2845106f5c2e227c64cb9ccc8
+
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -32,11 +31,11 @@ function getRandomInt(min, max) {
 }
 
 function rndmDog(){ 
-  var rndmsomthng = getRandomInt(1, 250)
+  var rndmsomthng = getRandomInt(0, 61)
   var requestUrl =
-    "https://api.thedogapi.com/v1/images/search?breeds_id="+rndmsomthng+"&api_key=3ad6ad84-85c0-4f2f-ad38-5b2e4d83c854";
+    "https://api.thedogapi.com/v1/images/search?breeds_id="+dogArrWorking[rndmsomthng]+"&api_key=3ad6ad84-85c0-4f2f-ad38-5b2e4d83c854";
 
-    
+
   fetch(requestUrl)
     .then(function (res) {
       return res.json();
@@ -69,11 +68,8 @@ function rndmDog(){
 function rndmCat() {
   var rndmsomthng = getRandomInt(1, 67)
   var requestUrl =
-<<<<<<< HEAD
-    "https://api.thecatapi.com/v1/images/search?breed_id="+rndmsomthng+"&api_key=f0f11a12-d477-4d44-ae91-5f773cb8183f";
-=======
-    "https://api.thecatapi.com/v1/images/search?breeds_id="+breedArr[rndmsomthng]+"&api_key=f0f11a12-d477-4d44-ae91-5f773cb8183f";
->>>>>>> 226e9dc53a94c8e2845106f5c2e227c64cb9ccc8
+    "https://api.thecatapi.com/v1/breeds/search?q="+breedArr[rndmsomthng]+"&api_key=f0f11a12-d477-4d44-ae91-5f773cb8183f";
+  console.log("hope this has all cat info",requestUrl)
 
   fetch(requestUrl)
     .then(function (res) {
@@ -103,10 +99,6 @@ function rndmCat() {
 rndmCat();
 rndmDog();
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 226e9dc53a94c8e2845106f5c2e227c64cb9ccc8
   homeMode.addEventListener('click', function() {
     window.location.href = "index.html"
 })
