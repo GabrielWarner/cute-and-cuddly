163bb50cf03ddf7936ddce086b95c320c804f708
GabrielWarner <gabe0412@uw.edu>
2022-07-09 13:27:57 -0700

    re arranged some code and made info display when user searches

--- dogScript.js
+++ dogScript.js
@@ -24,86 +24,83 @@ function renderCard(event){
 
   //initial fetch request
   fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      //clearing card each time it searches
-      breedCard.innerHTML = ""
-      console.log(data[0])
-      //setting the breed code to pass into function that will retrieve other info about dog
-      var breedCode = data[0].id
-      //creating div that will hold the image and secondary div that will hold the information
-      var div = document.createElement('div')
-      //creating to divs
-      var divLeft = document.createElement('div')
-      divLeft.setAttribute("class", "left col s6")
-      var divRight = document.createElement('div')
-      divRight.setAttribute("class", "right col s6")
-      div.setAttribute("class", "row")
-      //creating h3 element that holds the breeds name
-      var breedName = document.createElement('h3')
-      breedName.setAttribute("class", "col s12")
-      //grabbing the name from the data object 
-      breedName.textContent = data[0].name
-      //appending 
-      div.append(breedName)
-      breedCard.append(div)
-      breedCard.append(divLeft)
-      breedCard.append(divRight)
-      
-      //grab image id and then fetch image
-      function renderimage(data){
-        //with data passed in I am now grabbing the image ID and storing it in a variable and then building the new request url based off it
-        var imgID = data[0].reference_image_id
-        var requestUrl = 'https://api.thedogapi.com/v1/images/' + imgID;
-        
-        //fetch function that grabs image
-        fetch(requestUrl)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-            //grabbing image url and storing it in variable
-            imgURL = data.url
-            //creating image element so i can then set its src attribute to the URL i just made
-            var image = document.createElement("img")
-            image.setAttribute("src", imgURL)
-            image.setAttribute("class", "col s12")
-            //create div that sits inside of Breed Card and holds all of the do
-            var innerCard = document.createElement('div')
-            innerCard.textContent = 'TESTTEST'
-            innerCard.setAttribute("class", "col s6")
-            
-            divLeft.append(image)
-            divRight.append(innerCard)
-          });
-      }
-      renderimage(data)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    //clearing card each time it searches
+    breedCard.innerHTML = ""
+    console.log(data[0])
+    //creating div that will hold the image and secondary div that will hold the information
+    var div = document.createElement('div')
+    //creating two divs, left is to hold the image, right is to hold the info
+    var divLeft = document.createElement('div')
+    var divRight = document.createElement('div')
+    var cardTitle = document.createElement('h3')
+    //creating elements for info
+    var name = document.createElement("p")
+    var weight = document.createElement("p")
+    var height = document.createElement("p")
+    var bredFor = document.createElement("p")
+    var breedGroup = document.createElement("p")
+    var temperament = document.createElement("p")
+  
+  
+    //style
+    divLeft.setAttribute("class", "left col s6")
+    divRight.setAttribute("class", "right col s6")
+    div.setAttribute("class", "row")
+    cardTitle.setAttribute("class", "col s12")
+    cardTitle.textContent = data[0].name
+    name.textContent = 'Breed: ' + data[0].name
+    weight.textContent = 'Weight: ' + data[0].weight.imperial
+    height.textContent = 'Height: ' + data[0].height.imperial
+    bredFor.textContent = 'Bred For: ' + data[0].bred_for
+    breedGroup.textContent = 'Breed Group: ' + data[0].breed_group
+    temperament.textContent = 'Temperament: ' + data[0].temperament
+    //appending 
+    div.append(cardTitle)
+    breedCard.append(div)
+    breedCard.append(divLeft)
+    breedCard.append(divRight)
+    divRight.append(name)
+    divRight.append(weight)
+    divRight.append(bredFor)
+    divRight.append(breedGroup)
+    divRight.append(temperament)
 
-      //pass breed code into renderInfo function which builds search url off breed code and fethces the info
-      renderInfo(breedCode)
-      
-    });
-}
-
-function renderInfo(breedCode){
-  console.log(breedCode)
-  //use breed code to create URL
-  var secondUrl = "https://api.ThecatAPI.com/v1/images/search?breed_ids=beng&api_key=3ad6ad84-85c0-4f2f-ad38-5b2e4d83c854"
-
-  function getApi(secondUrl) {
-    fetch(secondUrl)
+        //grab image id and then fetch image
+  function renderimage(data){
+    //with data passed in I am now grabbing the image ID and storing it in a variable and then building the new request url based off it
+    var imgID = data[0].reference_image_id
+    var requestUrl = 'https://api.thedogapi.com/v1/images/' + imgID;
+    
+    //fetch function that grabs image
+    fetch(requestUrl)
       .then(function (response) {
         return response.json();
-    })
-    .then(function(data){
-      console.log(data)
-    });
+      })
+      .then(function (data) {
+        //grabbing image url and storing it in variable
+        imgURL = data.url
+        //creating image element so i can then set its src attribute to the URL i just made
+        var image = document.createElement("img")
+        image.setAttribute("src", imgURL)
+        image.setAttribute("class", "col s12")
+        //create div that sits inside of Breed Card and holds all of the do
+        var innerCard = document.createElement('div')
+        innerCard.setAttribute("class", "col s6")
+        
+        divLeft.append(image)
+        divRight.append(innerCard)
+      });
+      
   }
-  getApi(secondUrl)
-
+  //run the function to render image
+  renderimage(data)
+    })
 }
+
 
 
 
@@ -124,3 +121,9 @@ compBtn.addEventListener('click', function() {
 homeMode.addEventListener('click', function() {
   window.location.href = "index.html"
 })
+
+
+
+
+
+
