f0d2994470bc9b74f7b8943c3c962016c681e5f6
rasvindra <rasvindra@hotmail.com>
2022-07-09 12:45:27 -0700

    fixing

--- compare.js
+++ compare.js
@@ -7,6 +7,7 @@ var catsImage = document.getElementById("catImage");
 var catsName = document.getElementById("catNameApi");
 var catsLife = document.getElementById("catLifeApi");
 var catsInfo = document.getElementById("catInfoApi");
+var homeMode = document.getElementById('homeBtn')
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -16,7 +17,7 @@ function getRandomInt(min, max) {
 }
 
 function rndmDog(){ 
-  var rndmsomthng = getRandomInt(1, 60)
+  var rndmsomthng = getRandomInt(1, 250)
   var requestUrl =
     "https://api.thedogapi.com/v1/images/search?breeds_id="+rndmsomthng+"&api_key=3ad6ad84-85c0-4f2f-ad38-5b2e4d83c854";
 
@@ -53,7 +54,7 @@ function rndmDog(){
 function rndmCat() {
   var rndmsomthng = getRandomInt(1, 60)
   var requestUrl =
-    "https://api.thecatapi.com/v1/breeds/search?breeds_id="+rndmsomthng+"&api_key=3ad6ad84-85c0-4f2f-ad38-5b2e4d83c854";
+    "https://api.thecatapi.com/v1/images/search?breed_id="+rndmsomthng+"&api_key=f0f11a12-d477-4d44-ae91-5f773cb8183f";
 
   fetch(requestUrl)
     .then(function (res) {
@@ -74,7 +75,7 @@ function rndmCat() {
       newDiv3.textContent = data[0].temperament;
       catsInfo.append(newDiv3);
 
-      catsImage.setAttribute("src". data[0].url)
+      catsImage.setAttribute("src", data[0].url)
       //grab image id and then fetch image
 
     });
@@ -82,118 +83,6 @@ function rndmCat() {
 
 rndmCat();
 rndmDog();
-=======
-var dogsImage = document.getElementById('dogImage')
-var dogsName = document.getElementById('dogNameApi')
-var dogsLife = document.getElementById('dogLifeApi')
-var dogsInfo = document.getElementById('dogInfoApi')
-var catsImage = document.getElementById('catImage')
-var catsName = document.getElementById('catNameApi')
-var catsLife = document.getElementById('catLifeApi')
-var catsInfo = document.getElementById('catInfoApi')
-var homeMode = document.getElementById('homeBtn')
-
-
-function rndmDog(){
-
-    var dogAPI = "3ad6ad84-85c0-4f2f-ad38-5b2e4d83c854"
-    var requestUrl = 'https://api.thedogapi.com/v1/breeds/search?q=golden&api_key=3ad6ad84-85c0-4f2f-ad38-5b2e4d83c854';
-  
-    fetch(requestUrl)
-      .then(function (res) {
-        return res.json();
-      })
-      .then(function (data) {
-        console.log(data) //test
-
-        var newDiv = document.createElement('div')
-        newDiv.textContent = data[0].name
-        dogsName.append(newDiv)
-
-        var newDiv2 = document.createElement('div')
-        newDiv2.textContent = data[0].life_span
-        dogsLife.append(newDiv2)
-
-        var newDiv3 = document.createElement('div')
-        newDiv3.textContent = data[0].temperament
-        dogsInfo.append(newDiv3)
-
-
-        
-        //grab image id and then fetch image
-        function renderimage(data){
-          var imgID = data[0].reference_image_id
-         console.log(data) // test
-          
-          var requestUrl = 'https://api.thedogapi.com/v1/images/' + imgID;
-          
-          fetch(requestUrl)
-            .then(function (response) {
-              return response.json();
-            })
-            .then(function (data) {
-              imgURL = data.url
-              console.log(data)//  has URL
-              var image = document.createElement("img")
-              image.setAttribute("src", imgURL)
-              dogsImage.append(image)
-            });
-        }
-        renderimage(data)
-      });
-  }
-
-
-  function rndmCat(){
-    var dogAPI = "3ad6ad84-85c0-4f2f-ad38-5b2e4d83c854"
-    var requestUrl = 'https://api.thecatapi.com/v1/breeds/search?q=bengal&api_key=3ad6ad84-85c0-4f2f-ad38-5b2e4d83c854';
-  
-    fetch(requestUrl)
-      .then(function (res) {
-        return res.json();
-      })
-      .then(function (data) {
-        console.log(data) //test
-
-        var newDiv = document.createElement('div')
-        newDiv.textContent = data[0].name
-        catsName.append(newDiv)
-
-        var newDiv2 = document.createElement('div')
-        newDiv2.textContent = data[0].life_span
-        catsLife.append(newDiv2)
-
-        var newDiv3 = document.createElement('div')
-        newDiv3.textContent = data[0].temperament
-        catsInfo.append(newDiv3)
-
-
-        //grab image id and then fetch image
-        function renderimage(data){
-          var imgID = data[0].reference_image_id
-         console.log(data) //test
-          
-          var requestUrl = 'https://api.thecatapi.com/v1/images/' + imgID;
-          
-          fetch(requestUrl)
-            .then(function (response) {
-              return response.json();
-            })
-            .then(function (data) {
-              imgURL = data.url
-              console.log(data) // has URL
-              var image = document.createElement("img")
-              image.setAttribute("src", imgURL)
-              catsImage.append(image)
-            });
-        }
-        renderimage(data)
-      });
-  }
-
-
-  rndmCat()
-  rndmDog()
 
   homeMode.addEventListener('click', function() {
     window.location.href = "index.html"
