e74bb1e05ef657f38b1e9cb80be358aabc653e09
dhruza88 <davidhruza@gmail.com>
2022-07-11 17:09:54 -0700

    Adjusted call sequence, adjsuted storage for values, adjusted variable initialization, adjusted html button procedures, adjusted minor css

--- assets/compare/compare.js
+++ assets/compare/compare.js
@@ -1,88 +1,124 @@
-var dogsImage = document.getElementById("dogImage");
-var dogsName = document.getElementById("dogNameApi");
-var dogsLife = document.getElementById("dogLifeApi");
-var dogsInfo = document.getElementById("dogInfoApi");
-var catsImage = document.getElementById("catImage");
-var catsName = document.getElementById("catNameApi");
-var catsLife = document.getElementById("catLifeApi");
-var catsInfo = document.getElementById("catInfoApi");
-var homeMode = document.getElementById("homeBtn");
-var playMode = document.getElementById("playBtn");
-var breedArr = [
-  "Abyssinian",
-  "Aegean",
-  "American Bobtail",
-  "American Curl",
-  "American Shorthair",
-  "American Wirehair",
-  "Arabian Mau",
-  "Australian Mist",
-  "Balinese",
-  "Bambino",
-  "Bengal",
-  "Birman",
-  "Bombay",
-  "British Longhair",
-  "British Shorthair",
-  "Burmese",
-  "Burmilla",
-  "California Spangled",
-  "Chantilly-Tiffany",
-  "Chartreux",
-  "Chausie",
-  "Cheetoh",
-  "Colorpoint Shorthair",
-  "Cornish Rex",
-  "Cymric",
-  "Cyprus",
-  "Devon Rex",
-  "Donskoy",
-  "Dragon Li",
-  "Egyptian Mau",
-  "European Burmese",
-  "Exotic Shorthair",
-  "Havana Brown",
-  "Himalayan",
-  "Japanese Bobtail",
-  "Javanese",
-  "Khao Manee",
-  "Korat",
-  "Kurilian",
-  "LaPerm",
-  "Maine Coon",
-  "Malayan",
-  "Manx",
-  "Munchkin",
-  "Nebelung",
-  "Norwegian Forest Cat",
-  "Ocicat",
-  "Oriental",
-  "Persian",
-  "Pixie-bob",
-  "Ragamuffin",
-  "Ragdoll",
-  "Russian Blue",
-  "Savannah",
-  "Scottish Fold",
-  "Selkirk Rex",
-  "Siamese",
-  "Siberian",
-  "Singapura",
-  "Snowshoe",
-  "Somali",
-  "Sphynx",
-  "Tonkinese",
-  "Toyger",
-  "Turkish Angora",
-  "Turkish Van",
-  "York Chocolate",
-];
+const pleasePick = 'Please click on the Pet you think is the cutest';
+const youChoseCat = 'You picked the cat to be cutest';
+const youChoseDog = 'You picked the dog to be the cutest';
+
+var dogsImage = null;
+var dogsName = null;
+var dogsLife = null;
+var dogsInfo = null;
+var catsImage = null;
+var catsName = null;
+var catsLife = null;
+var catsInfo = null;
+var breedArr = [];
+let existingGame =
+  localStorage.getItem('petPicked') ?
+  JSON.parse(localStorage.getItem(`petPicked`)) :
+  {
+    youPickedTitle: pleasePick,
+    dog: 0,
+    cat: 0,
+  };
 
 function getRandomInt(min, max) {
    min = Math.ceil(min);
    max = Math.floor(max);
   
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+function init() {
+  this.initializeVariables();  
+  this.setTitleText(pleasePick);
+  this.callAnimalData();
+  console.log(existingGame);
+}
+
+function initializeVariables() {
+  dogsImage = document.getElementById("dogImage");
+  dogsName = document.getElementById("dogNameApi");
+  dogsLife = document.getElementById("dogLifeApi");
+  dogsInfo = document.getElementById("dogInfoApi");
+  catsImage = document.getElementById("catImage");
+  catsName = document.getElementById("catNameApi");
+  catsLife = document.getElementById("catLifeApi");
+  catsInfo = document.getElementById("catInfoApi");
+  breedArr = [
+    "Abyssinian",
+    "Aegean",
+    "American Bobtail",
+    "American Curl",
+    "American Shorthair",
+    "American Wirehair",
+    "Arabian Mau",
+    "Australian Mist",
+    "Balinese",
+    "Bambino",
+    "Bengal",
+    "Birman",
+    "Bombay",
+    "British Longhair",
+    "British Shorthair",
+    "Burmese",
+    "Burmilla",
+    "California Spangled",
+    "Chantilly-Tiffany",
+    "Chartreux",
+    "Chausie",
+    "Cheetoh",
+    "Colorpoint Shorthair",
+    "Cornish Rex",
+    "Cymric",
+    "Cyprus",
+    "Devon Rex",
+    "Donskoy",
+    "Dragon Li",
+    "Egyptian Mau",
+    "European Burmese",
+    "Exotic Shorthair",
+    "Havana Brown",
+    "Himalayan",
+    "Japanese Bobtail",
+    "Javanese",
+    "Khao Manee",
+    "Korat",
+    "Kurilian",
+    "LaPerm",
+    "Maine Coon",
+    "Malayan",
+    "Manx",
+    "Munchkin",
+    "Nebelung",
+    "Norwegian Forest Cat",
+    "Ocicat",
+    "Oriental",
+    "Persian",
+    "Pixie-bob",
+    "Ragamuffin",
+    "Ragdoll",
+    "Russian Blue",
+    "Savannah",
+    "Scottish Fold",
+    "Selkirk Rex",
+    "Siamese",
+    "Siberian",
+    "Singapura",
+    "Snowshoe",
+    "Somali",
+    "Sphynx",
+    "Tonkinese",
+    "Toyger",
+    "Turkish Angora",
+    "Turkish Van",
+    "York Chocolate",
+  ];
+}
+function setTitleText(titleVal) {
+  document.getElementById('cutestPet').innerText = titleVal;
+}
+function callAnimalData() {
+  this.rndmCat();
+  this.rndmDog();
 }
 
 function rndmDog() {
@@ -97,7 +133,7 @@ function rndmDog() {
       return res.json();
     })
     .then(function (data) {
-      console.log("dog", data); // test
+      // console.log("dog", data); // test
 
       if (data[0].breeds.length > 0) {
         var newDiv = document.createElement("div");
@@ -151,19 +187,20 @@ function rndmCat() {
     breedArr[rndmsomthng] +
     "&api_key=f0f11a12-d477-4d44-ae91-5f773cb8183f";
 
-  fetch(requestUrl)
+  try {
+    fetch(requestUrl)
     .then(function (res) {
       return res.json();
     })
     .then(function (data) {
-      console.log(data); //test
+      // console.log(data); //test
       catsImage.setAttribute("src", data[0].url);
       fetch(requestURL2)
         .then(function (res) {
           return res.json();
         })
         .then(function (data) {
-          console.log(data);
+          // console.log(data);
           var newDiv = document.createElement("div");
           newDiv.textContent = data[0].name;
           catsName.append(newDiv);
@@ -199,29 +236,48 @@ function rndmCat() {
       // catsImage.setAttribute("src", data[0].url)
       // //grab image id and then fetch image
     });
+  } catch(err) {
+    console.log('major failur fetching data');
+    setTimeout(() => {
+      this.rndmCat();
+    }, 1000)
+  }
 }
 
-rndmCat();
-rndmDog();
 
 function storeVar(value) {
-  var buttonEl = document.getElementById("picCompare");
-  console.log(buttonEl);
-  var cutePet = document.getElementById("cutestPet");
-  cutePet.innerHTML = value;
-  let amount = value;
-  console.log(amount);
+  let selTitle = '';
+  if (value === 0) {
+    existingGame.cat += 1;
+    selTitle = youChoseCat;
+  } else {
+    existingGame.dog += 1;
+    selTitle = youChoseDog;
+  }
+  localStorage.setItem('petPicked', JSON.stringify(existingGame));
 
-  // picCompare.classList.add("stopCursor");
-  buttonEl.classList.add("stopCursor");
+
+  this.setTitleText(selTitle);  
+  this.addRemoveStop(document.getElementById("picCompare"), true);
+  setTimeout(() => {
+    this.replay();
+  }, 5000);
+}
+
+function addRemoveStop(elmnt, stop) {
+  if (stop) {
+    elmnt.classList.add("stopCursor");
+  } else {
+    elmnt.classList.remove("stopCursor");
+  }
 }
 
 
 
-homeMode.addEventListener("click", function () {
+function goHome() {
   window.location.href = "index.html";
-});
+};
 
-playMode.addEventListener("click", function () {
-  window.location.href = "compare.html";
-});
+function replay() {
+  window.location.reload();
+};
