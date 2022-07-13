6c7bf29f1f251a68de7df8207c50d5ab669c1c77
alonpatashnik <alonp714@gmail.com>
2022-07-13 12:24:14 -0700

    commented code

--- assets/cat/catScript.js
+++ assets/cat/catScript.js
@@ -1,3 +1,6 @@
+//setting a bunch of variables to elements from our html
+//setting variables to urls of our apis
+//setting a variable to a random number between 0 and our breed array length - 1
 var cat = document.getElementById('catImage')
 var catFact = document.getElementById('catFact')
 var dogMode = document.getElementById('catBtn')
@@ -8,7 +11,11 @@ var inputEl = document.getElementById('breed-card-form')
 var searchInput = document.getElementById('search-input')
 var modalList = document.getElementById('modalList')
 var outColor = document.getElementById('outsideColor')
-
+var catUrl = 'https://api.thecatapi.com/v1/images/search?breed_ids='
+var catBreed = 'https://api.thecatapi.com/v1/breeds'
+var apiKey = '&api_key=f0f11a12-d477-4d44-ae91-5f773cb8183f'
+var randBreed = Math.floor(Math.random() * breedArr.length)
+var breedId = []
 var breedArr = ['Abyssinian', 'Aegean', 'American Bobtail', 'American Curl', 'American Shorthair',
 'American Wirehair', 'Arabian Mau', 'Australian Mist', 'Balinese', 'Bambino', 'Bengal', 'Birman',
 'Bombay', 'British Longhair', 'British Shorthair', 'Burmese', 'Burmilla', 'California Spangled',
@@ -21,17 +28,15 @@ var breedArr = ['Abyssinian', 'Aegean', 'American Bobtail', 'American Curl', 'Am
 'Snowshoe', 'Somali', 'Sphynx', 'Tonkinese', 'Toyger', 'Turkish Angora', 'Turkish Van', 'York Chocolate'
 ]
 
-var randBreed = Math.floor(Math.random() * breedArr.length)
-console.log(randBreed)
-var breedId = []
-var userSearch = 'beng'
-
+//loops through our breed array to generate list inside the modal
 for (var i = 0; i < breedArr.length; i++) {
     var breedLi = document.createElement('li')
     breedLi.innerHTML = breedArr[i]
     modalList.append(breedLi)
 }
 
+//runs a fetch from our breed list api to generate a matched list of breed ids alongside our breed names
+//called on page load
 function getBreedId() {
     fetch(catBreed)
         .then((response) => response.json())
@@ -40,24 +45,27 @@ function getBreedId() {
             var newId = data[i].id
             breedId.push(newId)
         }
-        console.log(breedId)
     })
     
     
 }
 
+//loads a random cat breed into the input field
+//called on page load
 function loadRand() {
     searchInput.value = breedArr[randBreed]
 }
 
-var catUrl = 'https://api.thecatapi.com/v1/images/search?breed_ids='
-var catBreed = 'https://api.thecatapi.com/v1/breeds'
-var apiKey = '&api_key=f0f11a12-d477-4d44-ae91-5f773cb8183f'
-
-
+//called on user submit in the input form
+//clears any image displayed on page
+//sets variable to the trimmed, lowercase value of what the user inputted when submit was hit
+//create new vadiable id
+//for every item in the breed array, compare the breed at that index to the breed the user entered
+//if we find a match, set the variable id to the item from breedId array at that index point
+//same for the breed and resets placeholder text to 'type breed'
+//if we dont find a match, display our error message as placeholder, clear anything in our data bio and end function
 function makeCatImage(event) {
     event.preventDefault()
-    console.log('hi')
     cat.innerHTML = ""
     var breed = searchInput.value.trim().toLowerCase()
     searchInput.value = ""
@@ -70,14 +78,15 @@ function makeCatImage(event) {
             searchInput.setAttribute('placeholder', "Type breed")
         }
     }
-    console.log(id)
     
     if (id === "") {
     searchInput.setAttribute('placeholder', "Not a breed: click button for list -->")
     catFact.innerHTML = ""
     return
     }
-    
+
+//adds the breed id to our api url that requires a breed id to search for images of that breed
+//dynamically creates image element using fetched url
     fetch(catUrl+id)
     .then((response) => response.json())
     .then((data) => {
@@ -89,11 +98,19 @@ function makeCatImage(event) {
         var elems = document.querySelectorAll('.materialboxed');
         var instances = M.Materialbox.init(elems);
     })
-    console.log(breed)
 
+//call next function, passing in our breed, to generate the bio card
     makeCatDataCard(breed)
 }
 
+
+//function that generates the bio card
+//run a fetch to generate our list of cat breeds and data about each cat
+//takes in the breed generated from makeCatImage() and passes it into a conditional statement within a for loop.
+//if the breed from makeCatImage() matches any breed in our generated data array from the fetch, we dynamically create
+//a bunch of elements that make up the data card and styles them with other data from the breed at the index point that satisfied our if statement
+//at the end of the function it sets the background of our whole static container to green
+//called inside makeCatImage()
 function makeCatDataCard(breed) {
     catFact.innerHTML = ""
     fetch(catBreed)
@@ -155,28 +172,39 @@ function makeCatDataCard(breed) {
     })
 }
 
+// functions that run on page load to generate out breed id list and load a random 
 getBreedId()
 loadRand()
 
+//jquery that triggers modal
 $(document).ready(function(){
     $('.modal').modal();
 });
 
+// event listener that takes us to dog page on button click
 dogMode.addEventListener('click', function() {
     window.location.href = "doghtml.html"
 })
 
+// event listener that takes us to cat page on button click
 compBtn.addEventListener('click', function() {
     window.location.href = 'compare.html'
 })
 
+// event listener that takes us to home page on button click
 homeMode.addEventListener('click', function() {
     window.location.href = "index.html"
 })
 
+
 inputEl.addEventListener('submit', makeCatImage)
 
-// THIS WAS ALL ORIGINAL TESTING THAT WE USED TO LEARN ABOUT API
+
+
+
+
+// THIS WAS ALL ORIGINAL TESTING THAT WE USED TO LEARN ABOUT API AND TRY TO GENERATE A DROPDOWN
+// var userSearch = 'beng'
 // $('.dropdown-trigger').dropdown();
 // function catPicByBreed() {
     
